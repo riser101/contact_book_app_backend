a4b443a671cd377f3e09d12ed75dd7e05bb7d55d
yousuf syed <sy.yousuf9106@gmail.com>
2018-04-30 17:06:37 +0530

    added production envs

--- app/__init__.py
+++ app/__init__.py
@@ -1,40 +1,34 @@
-# third-party imports
+
 from flask import Flask
-# from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-
-# local imports
 from config import app_config
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 ma = Marshmallow()
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-    
-    # Bootstrap(app)
+    if(os.getenv('FLASK_CONFIG')=='production') :
+        app = Fask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
-    # login_manager.login_view = "auth.login"
     migrate = Migrate(app, db)
-
     from app import models
-
-    # from .admin import admin as admin_blueprint
     from .operations import operations as ops_blueprint
     app.register_blueprint(ops_blueprint)
-
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
-
-    # from .home import home as home_blueprint
-    # app.register_blueprint(home_blueprint)
-
     return app
